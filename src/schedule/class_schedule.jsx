708cadcee5b1fb047183565b9f2aace72cfedc5c
mrdcvlsc <mrdcvlsc@gmail.com>
2025-02-11 22:26:27 +0800

    isolate time slot lable generation function

--- src/schedule/class_schedule.jsx
+++ src/schedule/class_schedule.jsx
@@ -8,46 +8,9 @@ import "./TimeTable.css";
 import "./TimeTableDropdowns.css";
 
 import { deserialize_schedule, fetch_all_departments, fetch_class_json_schedule, fetch_department_data, fetch_serialized_class_schedule } from "../js/schedule"
+import { generateTimeSlotRowLabels } from "../js/week-time-table-grid-functions";
 
 const SECTION_CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890abcdefghijklmnopqrstuvwxyz";
-
-const formatTime = (hour, minutes) => {
-  const period = hour >= 12 ? "PM" : "AM";
-  const unformatted_hour = hour % 12 === 0 ? 12 : hour % 12;
-  const formattedHour = String(unformatted_hour).padStart(2, "0");
-  const formattedMinutes = String(minutes).padStart(2, "0");
-  return `${formattedHour}:${formattedMinutes} ${period}`;
-};
-
-function generateTimeSlotRowLabels(startHour, minuteIntervals, dailyTimeSlots) {
-  const time_slots = [];
-  let hour = startHour;
-  let minutes = 0;
-
-  for (let i = 0; i < dailyTimeSlots; i++) {
-    const startTime = formatTime(hour, minutes);
-
-    let endHour = hour;
-    let endMinutes = minutes + minuteIntervals;
-
-    if (endMinutes >= 60) {
-      endHour += Math.floor(endMinutes / 60);
-      endMinutes %= 60;
-    }
-
-    const endTime = formatTime(endHour, endMinutes);
-
-    time_slots.push(`${startTime} - ${endTime}`);
-
-    minutes += minuteIntervals;
-    if (minutes >= 60) {
-      hour++;
-      minutes %= 60;
-    }
-  }
-
-  return time_slots;
-};
 
 function TimeTable() {
 
