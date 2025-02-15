a1ed4c21160ee41b17d754d1b4a86b2df3421265
mrdcvlsc <mrdcvlsc@gmail.com>
2025-02-15 22:05:22 +0800

    implement editing instructor time slot availability

--- src/instructors/instructors.jsx
+++ src/instructors/instructors.jsx
@@ -1,4 +1,4 @@
-import { StrictMode, useState, useEffect, useRef } from "react";
+import { StrictMode, useState, useEffect } from "react";
 import { createRoot } from "react-dom/client";
 
 import { Loading, Popup } from "../components/Loading";
@@ -8,9 +8,10 @@ import "./TimeTable.css";
 import "./TimeTableDropdowns.css";
 import "./instructors.css";
 
-import { fetch_all_departments, fetch_department_instructors_erd, fetch_department_instructors_era } from "../js/schedule"
+import { fetch_all_departments, fetch_department_instructors_erd, fetch_department_instructors_era, post_update_insturctor } from "../js/schedule"
 import { generateTimeSlotRowLabels } from "../js/week-time-table-grid-functions";
 import { InstructorTimeSlotBitMap } from "../js/instructor-time-slot-bit-map"
+import { ContextMenu, ContextMenuItem, Position, useContextMenuState } from "../components/ContextMenu";
 
 function TimeTable() {
 
@@ -34,9 +35,9 @@ function TimeTable() {
   //                       STATES FOR FETCHED DATA
   /////////////////////////////////////////////////////////////////////////////////
 
-  const [allDepartment, setAllDepartment] = useState([]);                // fetch on page load
-  const [instructorERD, setInstructorERD] = useState([]);                // fetch on page load
-  const [instructorERA, setInstructorERA] = useState([]);                // fetch on page load
+  const [allDepartment, setAllDepartment] = useState([]); // fetch on page load
+  const [instructorsERD, setInstructorsERD] = useState([]); // fetch on page load
+  const [instructorsERA, setInstructorsERA] = useState([]); // fetch on page load
 
   /////////////////////////////////////////////////////////////////////////////////
   //                       DROPDOWN SELECTION STATES
@@ -49,9 +50,16 @@ function TimeTable() {
   //                       SELECTED INSTRUCTORS
   /////////////////////////////////////////////////////////////////////////////////
 
-  const [instructorIndex, setInstructorIndex] = useState("")
-  const [instructorAlloc, setInstructorAlloc] = useState("")
-  const [instructorDefault, setInstructorDefault] = useState("")
+  const [selectedInstructorIndex, setSelectedInstructorIndex] = useState("")
+  const [selectedInstructorAlloc, setSelectedInstructorAlloc] = useState("")
+  const [selectedInstructorDefault, setSelectedInstructorDefault] = useState("")
+
+  /////////////////////////////////////////////////////////////////////////////////
+  //                       SELECTED TIME SLOT CELL
+  /////////////////////////////////////////////////////////////////////////////////
+
+  const [selectedTimeSlots, setSelectedTimeSlots] = useState(new Set())
+  const [isDragSelect, setIsDragSelect] = useState()
 
   /////////////////////////////////////////////////////////////////////////////////
   //                       PAGE LOAD PROCESS
@@ -107,11 +115,21 @@ function TimeTable() {
     console.log(`selected departmentID: ${event.target.value}`);
     setDepartmentID(event.target.value);
     setSemesterIndex("");
+
+    setInstructorsERD([])
+    setInstructorsERA([])
+
+    setSelectedInstructorIndex("")
+    setSelectedInstructorAlloc("")
+    setSelectedInstructorDefault("")
+
+    setSelectedTimeSlots(new Set([]))
   }
 
   const handleSemesterChange = async (event) => {
     console.log(`selected semesterIndex: ${event.target.value}`);
     setSemesterIndex(event.target.value);
+    setSelectedTimeSlots(new Set([]))
 
     try {
       setIsLoading(true);
@@ -127,12 +145,12 @@ function TimeTable() {
         instructors_era[i].Time = new InstructorTimeSlotBitMap(instructors_era[i].Time);
       }
 
-      setInstructorERD(instructors_erd)
+      setInstructorsERD(instructors_erd)
       console.log('instructors_erd')
       console.log(instructors_erd);
       console.log()
 
-      setInstructorERA(instructors_era)
+      setInstructorsERA(instructors_era)
       console.log('instructors_era')
       console.log(instructors_era);
       console.log()
@@ -150,14 +168,12 @@ function TimeTable() {
   };
 
   const handleInstructorSelection = (instructor_erd_index) => {
-    console.log('selected instructor index =', instructor_erd_index)
-    console.log('instructorERD[selected_index].Time:')
-    console.log(instructorERD[instructor_erd_index].Time)
+    setSelectedTimeSlots(new Set([]))
 
     let allocated_instructors_idx = -1
 
-    for (let i = 0; i < instructorERA.length; i++) {
-      if (instructorERD[instructor_erd_index].InstructorID === instructorERA[i].InstructorID) {
+    for (let i = 0; i < instructorsERA.length; i++) {
+      if (instructorsERD[instructor_erd_index].InstructorID === instructorsERA[i].InstructorID) {
         allocated_instructors_idx = i
         break
       }
@@ -170,18 +186,116 @@ function TimeTable() {
         Message: "missing default instructor encoding resources"
       });
     } else {
-      setInstructorIndex(instructor_erd_index)
-      setInstructorDefault(instructorERD[instructor_erd_index])
-      setInstructorAlloc(instructorERA[allocated_instructors_idx])
+      setSelectedInstructorIndex(instructor_erd_index)
+      setSelectedInstructorDefault(instructorsERD[instructor_erd_index])
+      setSelectedInstructorAlloc(instructorsERA[allocated_instructors_idx])
+
+      console.log('selected instructor index =', instructor_erd_index)
+
+      console.log('instructorERD[selected_index].Time :')
+      console.log(instructorsERD[instructor_erd_index])
+
+      console.log('instructorsERA[allocated_instructors_idx] :')
+      console.log(instructorsERA[allocated_instructors_idx])
     }
+  }
+
+  /////////////////////////////////////////////////////////////////////////////////
+  //                     TIME SLOT SELECTION BUTTON HANDLERS
+  /////////////////////////////////////////////////////////////////////////////////
+
+  const handleApplyTimeSlotDefaultChanges = async () => {
+    try {
+
+      let new_default_time = []
+
+      for (let i = 0; i < selectedInstructorDefault.Time.bitset.length; i++) {
+        new_default_time.push(`${selectedInstructorDefault.Time.bitset[i]}`)
+      }
+
+      const updated_instructor_time_str = {
+        InstructorID: selectedInstructorDefault.InstructorID,
+        DepartmentID: selectedInstructorDefault.DepartmentID,
+        FirstName: selectedInstructorDefault.FirstName,
+        MiddleInitial: selectedInstructorDefault.MiddleInitial,
+        LastName: selectedInstructorDefault.LastName,
+        Time: new_default_time,
+      }
+
+      setIsLoading(true);
+
+      await post_update_insturctor(updated_instructor_time_str);
+      
+      setIsLoading(false);
+
+    } catch (err) {
+      setPopupOptions({
+        Heading: "Instructor Update Failed",
+        HeadingStyle: { background: "red", color: "white" },
+        Message: `${err}`
+      });
+      setIsLoading(false);
+    }
+  }
+
+  const handleContextMenuEnable = () => {
+    console.log('before enable =', selectedInstructorDefault)
+
+    for (const value of selectedTimeSlots) {
+      let [day, time_slot] = value.split(":")
+
+      day = Number(day)
+      time_slot = Number(time_slot)
+
+      if (!selectedInstructorDefault?.Time?.getAvailability(day, time_slot)) {
+        selectedInstructorDefault.Time.setAvailability(true, day, time_slot)
+        selectedInstructorAlloc.Time.setAvailability(true, day, time_slot)
+      }
+    }
+
+    setSelectedInstructorAlloc(selectedInstructorAlloc)
+    setSelectedInstructorDefault(selectedInstructorDefault)
+
+    setSelectedTimeSlots(new Set())
+  }
+
+  const handleContextMenuDisable = () => {
+    console.log('before disable =', selectedInstructorDefault)
+
+    for (const value of selectedTimeSlots) {
+      let [day, time_slot] = value.split(":")
+
+      day = Number(day)
+      time_slot = Number(time_slot)
+
+      if (selectedInstructorDefault?.Time?.getAvailability(day, time_slot)) {
+        selectedInstructorDefault.Time.setAvailability(false, day, time_slot)
+        selectedInstructorAlloc.Time.setAvailability(false, day, time_slot)
+      }
+    }
+
+    setSelectedInstructorAlloc(selectedInstructorAlloc)
+    setSelectedInstructorDefault(selectedInstructorDefault)
+
+    setSelectedTimeSlots(new Set())
   }
 
   /////////////////////////////////////////////////////////////////////////////////
   //                              COMPONENT UI CODE
   /////////////////////////////////////////////////////////////////////////////////
 
+  const contextMenuState = useContextMenuState()
+
   return (
     <>
+      <ContextMenu
+        closeAfterClick={true}
+        conextMenuState={contextMenuState}
+      >
+        <ContextMenuItem onClick={handleContextMenuEnable}>Enable</ContextMenuItem>
+        <ContextMenuItem onClick={handleContextMenuDisable}>Disable</ContextMenuItem>
+      </ContextMenu>
+
       {/*================================= Loading Component =================================*/}
 
       <Popup popupOptions={popupOptions} closeButtonActionHandler={() => {
@@ -219,11 +333,11 @@ function TimeTable() {
           <div className="instructor-list">
             <h2>General Instructors</h2>
             <div className="instructor-list-items">
-              {instructorERD?.map((instructor, index) => {
+              {instructorsERD?.map((instructor, index) => {
                 if (instructor.DepartmentID === 0) {
                   return <div
                     key={index}
-                    className={`instructor-item ${instructorIndex === index ? "selected" : ""}`}
+                    className={`instructor-item ${selectedInstructorIndex === index ? "selected" : ""}`}
                     onClick={() => handleInstructorSelection(index)}
                   >
                     <div className="instructor-ids">{`${instructor.InstructorID}`}</div>
@@ -239,11 +353,11 @@ function TimeTable() {
           <div className="instructor-list">
             <h2>Department Instructors</h2>
             <div className="instructor-list-items">
-              {instructorERD?.map((instructor, index) => {
+              {instructorsERD?.map((instructor, index) => {
                 if (instructor.DepartmentID === Number(departmentID)) {
                   return <div
                     key={index}
-                    className={`instructor-item ${instructorIndex === index ? "selected" : ""}`}
+                    className={`instructor-item ${selectedInstructorIndex === index ? "selected" : ""}`}
                     onClick={() => handleInstructorSelection(index)}
                   >
                     <div className="instructor-ids">{`${instructor.InstructorID}`}</div>
@@ -260,6 +374,21 @@ function TimeTable() {
 
         {/*================================= TimeTable Table =================================*/}
 
+        <div style={{ width: "100%", display: "flex", justifyContent: "space-between", alignItems: "center", paddingInline: "1em" }}>
+          <div style={{ display: "flex", flexDirection: "row", justifyContent: "center", alignItems: "center", gap: "0.6em" }}>
+            <p>time slot drag select </p>
+            {isDragSelect ?
+              <div style={{ width: "0.8em", height: "0.8em", background: "green" }}></div> :
+              <div style={{ width: "0.8em", height: "0.8em", background: "red" }}></div>
+            }
+          </div>
+          <div style={{ display: "flex", gap: "0.8em" }}>
+            <button className="all-btns" onClick={() => setSelectedTimeSlots(new Set([]))}>Clear Time Slot Selection</button>
+            <button className="all-btns" onClick={handleApplyTimeSlotDefaultChanges}>Apply Changes</button>
+            <button className="all-btns">Cancel</button>
+          </div>
+        </div>
+
         <table className="time-table">
           <thead>
             <tr>
@@ -274,36 +403,84 @@ function TimeTable() {
               <tr key={time_slot_index}>
                 <td className="time-slot">{time_slot_label}</td>
                 {DAYS.map((_, day_index) => {
-                  if (instructorDefault) {
-                    const is_enabled_time_slot = instructorDefault.Time.getAvailability(day_index, time_slot_index)
+                  let class_name = ""
+                  let selected = ""
 
-                    if (!is_enabled_time_slot) {
-                      return <td key={day_index} className="disabled-slot" onClick={() => {
-                        const available = instructorAlloc.Time.getAvailability(day_index, time_slot_index)
-                        console.log(`day(${day_index}), time_slot(${time_slot_index} = available? ${available})`)
-                      }} />
-                    }
+                  const is_available_default = selectedInstructorDefault?.Time?.getAvailability(day_index, time_slot_index) ? true : false
+                  const is_available_alloc = selectedInstructorAlloc?.Time?.getAvailability(day_index, time_slot_index) ? true : false
 
-                    if (instructorAlloc) {
-                      const is_available_time_slot = instructorAlloc.Time.getAvailability(day_index, time_slot_index)
-
-                      if (!is_available_time_slot) {
-                        return <td key={day_index} className="occupied-slot" onClick={() => {
-                          const available = instructorAlloc.Time.getAvailability(day_index, time_slot_index)
-                          console.log(`day(${day_index}), time_slot(${time_slot_index} = available? ${available})`)
-                        }}></td>;
-                      }
-
-                      return (
-                        <td key={day_index} className="available-slot" onClick={() => {
-                          const available = instructorAlloc.Time.getAvailability(day_index, time_slot_index)
-                          console.log(`day(${day_index}), time_slot(${time_slot_index} = available? ${available})`)
-                        }} />
-                      );
-                    }
+                  if (!is_available_default) {
+                    class_name = "disabled-slot"
+                  } else if (is_available_default && !is_available_alloc) {
+                    class_name = "occupied-slot"
                   } else {
-                    return <td key={day_index} className="disabled-slot" />
+                    class_name = "available-slot"
                   }
+
+                  if (selectedTimeSlots?.has(`${day_index}:${time_slot_index}`)) {
+                    selected = "selected-time-slot-cell"
+                  }
+
+                  return (
+                    <td
+                      key={day_index}
+                      className={class_name}
+
+                      onContextMenu={(event) => {
+                        event.preventDefault()
+                        console.log(`right click: class="${event.target.className}"`)
+
+                        const available = selectedInstructorAlloc?.Time?.getAvailability(day_index, time_slot_index)
+                        console.log(`day(${day_index}), time_slot(${time_slot_index} = available? ${available})`)
+
+                        contextMenuState.setShow(true)
+                        contextMenuState.setPosition(new Position(event.clientX, event.clientY))
+
+                        const is_selected = selectedTimeSlots.has(`${day_index}${time_slot_index}`)
+
+                        if (!is_selected) {
+                          const new_selected_time_slots = new Set(selectedTimeSlots)
+                          new_selected_time_slots.add(`${day_index}:${time_slot_index}`)
+                          setSelectedTimeSlots(new_selected_time_slots)
+                          console.log(new_selected_time_slots)
+                        }
+                      }}
+
+                      onMouseDown={(event) => {
+                        console.log(`drag start: class="${event.target.className}"`)
+                        setIsDragSelect(true)
+
+                        const is_selected = selectedTimeSlots.has(`${day_index}${time_slot_index}`)
+
+                        if (!is_selected) {
+                          const new_selected_time_slots = new Set(selectedTimeSlots)
+                          new_selected_time_slots.add(`${day_index}:${time_slot_index}`)
+                          setSelectedTimeSlots(new_selected_time_slots)
+                          console.log(new_selected_time_slots)
+                        }
+                      }}
+
+                      onMouseEnter={(event) => {
+                        console.log(`dragging: class="${event.target.className}"`)
+
+                        const is_selected = selectedTimeSlots.has(`${day_index}${time_slot_index}`)
+
+                        if (!is_selected && isDragSelect) {
+                          const new_selected_time_slots = new Set(selectedTimeSlots)
+                          new_selected_time_slots.add(`${day_index}:${time_slot_index}`)
+                          setSelectedTimeSlots(new_selected_time_slots)
+                          console.log(new_selected_time_slots)
+                        }
+                      }}
+
+                      onMouseUp={(event) => {
+                        console.log(`drag end: class="${event.target.className}"`)
+                        setIsDragSelect(false)
+                      }}
+                    >
+                      <span className={`time-slot-cover ${selected}`}></span>
+                    </td>
+                  )
                 })}
               </tr>
             ))}
