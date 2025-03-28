1a6b2231f8f9443ef8430c1e3eac2f61d2f71163
mrdcvlsc <mrdcvlsc@gmail.com>
2025-03-28 22:11:43 +0800

    remove App.css import in about page

--- src/about/about.jsx
+++ src/about/about.jsx
@@ -4,7 +4,6 @@ import { createRoot } from 'react-dom/client'
 import reactLogo from '../assets/react.svg'
 import viteLogo from '../assets/vite.svg'
 
-import '../App.css'
 import '../index.css'
 
 function About() {
