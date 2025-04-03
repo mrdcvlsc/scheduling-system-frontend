0b9cd303e72db9d441e01149bf5d32fc18b7abac
mrdcvlsc <mrdcvlsc@gmail.com>
2025-04-04 07:54:44 +0800

    use local network ip address

--- src/js/basics.js
+++ src/js/basics.js
@@ -3,8 +3,8 @@ const API_VERSION = 'v1'
 // this is for development only : start.
 const DEV = true
 
-const base_url = 'http://localhost:3000'
-// const base_url = 'http://192.168.1.17:3000'
+// const base_url = 'http://localhost:3000'
+const base_url = 'http://192.168.1.20:3000' // my binded local server's network ip
 
 // this is for development only : end.
 
